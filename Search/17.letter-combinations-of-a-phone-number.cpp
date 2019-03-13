1c655dbda13cfd0e00f406f75750b2e468ec62f4
AnandrewChan <andrew_rong@163.com>
2019-03-13 16:34:25 +0800

    search

---
+++ Search/17.letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=17 lang=cpp
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (40.57%)
+ * Total Accepted:    353.8K
+ * Total Submissions: 872K
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * Note:
+ * 
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ * 
+ */
+class Solution
+{
+  public:
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty())
+            return {};
+        vector<string> an{""};
+        string dict[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (int i = 0; i < digits.size(); i++)
+        {
+            vector<string> t;
+            string str = dict[digits[i] - '2'];
+            for (int j = 0; j < str.size(); j++)
+            {
+                for (string s : an)
+                    t.push_back(s + str[j]);
+            }
+            an = t;
+        }
+        return an;
+    }
+};
---
+++ Search/[O]39.combination-sum.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=39 lang=cpp
+ *
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (46.95%)
+ * Total Accepted:    310.8K
+ * Total Submissions: 662K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * Given a set of candidate numbers (candidates) (without duplicates) and a
+ * target number (target), find all unique combinations in candidates where the
+ * candidate numbers sums to target.
+ * 
+ * The same repeated number may be chosen from candidates unlimited number of
+ * times.
+ * 
+ * Note:
+ * 
+ * 
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * 
+ * 
+ */
+
+/*
+Runtime: 92 ms
+Memory Usage: 21.8 MB
+*/
+class Solution
+{
+  public:
+    void search(vector<int> &candidates, int pos, int target, vector<int> temp, vector<vector<int>> &an)
+    {
+        if (target == 0)
+        {
+            an.push_back(temp);
+            return;
+        }
+        if (pos >= candidates.size())
+            return;
+        int c = target / candidates[pos];
+        for (int i = 0; i <= c; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                temp.push_back(candidates[pos]);
+            }
+            search(candidates, pos + 1, target - i * candidates[pos], temp, an);
+            for (int j = 0; j < i; j++)
+            {
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        sort(candidates.begin(), candidates.end());
+        vector<int> temp;
+        vector<vector<int>> an;
+        search(candidates, 0, target, temp, an);
+        return an;
+    }
+};
